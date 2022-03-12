26a9e5eae21f992cbb51f32e3a84be1fadaf4eb9
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-12 21:46:32 +0800

    Delete recovery directory

--- recovery/bootloader.c
+++
@@ -1,7 +0,0 @@
-#include "bootloader.h"
-int get_bootloader_message(struct bootloader_message *out) {
- 	return 0;
-}
-int set_bootloader_message(const struct bootloader_message *in){
- 	return 0;
-}
